ce6111ad4abac39f261df88206fcd75390fe251d
Sofia Basile <sofibasile86@gmail.com>
2022-12-07 10:00:59 -0300

    desafioprefinal

---
+++ IntegrandoApisConAdoNet/Controllers/ProductoVendidoController.cs
@@ -0,0 +1,20 @@
+﻿using IntegrandoApisConAdoNet.Model;
+using IntegrandoApisConAdoNet.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegrandoApisConAdoNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductoVendidoController : ControllerBase
+    {
+
+        //Traer Productos vendidos
+        [HttpGet(Name = "GetProductosVendidos")]
+        public List<ProductoVendido> GetProductosVendidos()
+        {
+            return ProductoVendidoHandler.GetProductosVendidos();
+        }
+
+    }
+}
---
+++ IntegrandoApisConAdoNet/Controllers/VentaController.cs
@@ -0,0 +1,43 @@
+﻿using IntegrandoApisConAdoNet.Controllers.DTOS;
+using IntegrandoApisConAdoNet.Model;
+using IntegrandoApisConAdoNet.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegrandoApisConAdoNet.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class VentaController : ControllerBase
+    {
+
+        //Traer Venta
+        [HttpGet]
+        public List<Venta> GetVentas()
+        {
+            return VentaHandler.GetVentas();
+        }
+
+        //Cargar Venta
+        [HttpPost]
+        public bool CargarVenta([FromBody] PostVenta venta)
+        {
+            try
+            {
+                return VentaHandler.CargarVenta(new Venta
+                {
+                    
+                    Comentarios = venta.Comentarios,
+                    IdUsuario = venta.IdUsuario
+
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
